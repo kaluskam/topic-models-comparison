380a498b1341f9bd95fd0a2c30bbfa4091fcf651
Marcelina Kurek <marcelina.kurek1@gmail.com>
2022-11-07 09:15:40 +0100

    add metrics

--- metrics/coherence_metric.py
+++ metrics/coherence_metric.py
@@ -3,6 +3,7 @@ from metrics.metric import Metric
 
 from gensim.models.coherencemodel import CoherenceModel
 from gensim.corpora.dictionary import Dictionary
+from octis.evaluation_metrics.coherence_metrics import WECoherencePairwise, WECoherenceCentroid
 
 
 class UMassCoherenceMetric(Metric):
@@ -103,3 +104,54 @@ class CNPMICoherenceMetric(Metric):
 
     def init_default_parameters(self):
         self.parameters = {"coherencemodel": {}}
+
+
+class WECoherencePairwiseMetric(Metric):
+    def __init__(self, flag=False, range=(-1, 1), parameters=None):
+        super().__init__(flag, range, parameters)
+        if parameters is None:
+            self.init_default_parameters()
+
+    def evaluate(self, inputData, outputData):
+        super().evaluate(inputData, outputData)
+        coherence_pairwise = WECoherencePairwise(**self.parameters["coherencemodel"])
+        topics_dict = {"topics": outputData.get_topics()}
+        return coherence_pairwise.score(topics_dict)
+
+    def init_default_parameters(self):
+        """
+        Parameters
+        ----------
+        dictionary with keys
+        topk : how many most likely words to consider
+        word2vec_path : if word2vec_file is specified retrieves word embeddings file (in word2vec format)
+        to compute similarities, otherwise 'word2vec-google-news-300' is downloaded
+        binary : True if the word2vec file is binary, False otherwise (default False)
+        """
+        self.parameters = {"coherencemodel": {}}
+
+
+class WECoherenceCentroidMetric(Metric):
+    def __init__(self, flag=False, range=(-1, 1), parameters=None):
+        super().__init__(flag, range, parameters)
+        if parameters is None:
+            self.init_default_parameters()
+
+    def evaluate(self, inputData, outputData):
+        super().evaluate(inputData, outputData)
+        coherence_centroid = WECoherenceCentroid(**self.parameters["coherencemodel"])
+        topics_dict = {"topics": outputData.get_topics()}
+        return coherence_centroid.score(topics_dict)
+
+    def init_default_parameters(self):
+        """
+        Parameters
+        ----------
+        dictionary with keys
+        topk : how many most likely words to consider
+        word2vec_path : if word2vec_file is specified retrieves word embeddings file (in word2vec format)
+        to compute similarities, otherwise 'word2vec-google-news-300' is downloaded
+        binary : True if the word2vec file is binary, False otherwise (default False)
+        """
+        self.parameters = {"coherencemodel": {}}
+
