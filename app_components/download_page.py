c7718d77089cd66fa8aeca747825b6f75abb072e
Mikołaj Spytek <mikolajspytek@gmail.com>
2022-11-23 13:53:46 +0100

    fixed work in progress message on download page

--- app_components/download_page.py
+++ app_components/download_page.py
@@ -4,5 +4,5 @@ from dash import html
 dash.register_page(__name__, path='/download-subreddits')
 
 layout = html.Div(
-    html.H1("Tu będziemy pobierać dane")
+    html.H1("The data downloading module is not integrated with the app yet. Please use the commandline to download new subreddits.")
 )
