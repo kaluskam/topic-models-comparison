c43820505d2f735f64073adb0288f9e4ebee5e3b
Marcelina Kurek <marcelina.kurek1@gmail.com>
2022-12-10 00:42:46 +0100

    change cache data

--- definitions.py
+++ definitions.py
@@ -5,7 +5,7 @@ import datetime as dt
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 RAW_DIR = os.path.join(ROOT_DIR, 'data', 'raw')
 PREPROCESSED_DIR = os.path.join(ROOT_DIR, 'data', 'preprocessed')
-CACHE_DIR = os.path.join(ROOT_DIR, 'data', 'cache')
+CACHE_DIR = os.path.join(ROOT_DIR, 'output', 'cache')
 
 START_DATE = dt.date(2019, 10, 1)
 END_DATE = dt.date(2022, 9, 30)
