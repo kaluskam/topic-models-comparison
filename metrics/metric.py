9f6fb280a4f22dc497392301b8990090b7efa26e
Marcelina Kurek <marcelina.kurek1@gmail.com>
2022-12-20 20:59:43 +0100

    add docstrings (metrics folder)

--- metrics/metric.py
+++ metrics/metric.py
@@ -15,10 +15,23 @@ class Metric:
         self.range = range
     
     def evaluate(self, inputData, outputData):
+        """
+        Calculate the score of a metric
+
+        Parameters
+        ----------
+        inputData : InputData
+            input data of a model
+        outputData: OutputData
+            output data from a model
+        """
         assert type(inputData) == InputData
         assert type(outputData) == OutputData 
 
     def init_default_parameters(self):
+        """
+        Default parameters initialization if no parameters were specified
+        """
         pass
 
 
