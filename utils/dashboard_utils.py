6f0d0d43081fc155316ece655c6b5ba2aead942b
Marcelina Kurek <marcelina.kurek1@gmail.com>
2022-12-20 14:13:11 +0100

    add docstrings (utils folder)

--- utils/dashboard_utils.py
+++ utils/dashboard_utils.py
@@ -3,10 +3,26 @@ import definitions as d
 
 
 def get_available_subreddits():
+    """
+    Retrieve available (already downloaded) subreddits
+
+    Returns
+    -------
+    list
+        a list of already downloaded subreddits (present in the raw directory data)
+    """
     return [subreddit.replace('.csv', '') for subreddit in os.listdir(d.RAW_DIR)]
 
 
 def get_data_for_subreddit_select():
+    """
+    Retrieve available subreddits
+
+    Returns
+    -------
+    dictionary
+        a dictionary of downloaded subreddits (present in the raw directory data)
+    """
     subreddits = get_available_subreddits()
     result = []
     for subreddit in subreddits:
--- utils/data_loading.py
+++ utils/data_loading.py
@@ -10,6 +10,21 @@ from pathlib import Path
 
 
 def load_downloaded_data(subreddits, date_range):
+    """
+    Loads data from already downloaded subreddits for specified date range
+
+    Parameters
+    ----------
+    subreddits : a list of strings
+        subreddit names to be loaded
+    date_range: list of DateTime objects
+        indicates the first and the last date of a period to be loaded
+
+    Returns
+    -------
+    input_data : InputData
+        merged data for given subreddits and date_range
+    """
     dfs = []
     subreddits.sort()
 
@@ -30,20 +45,80 @@ def load_downloaded_data(subreddits, date_range):
 
 
 def create_output_data_cache_filepath(subreddit, date_range, model_alias):
+    """
+    Create an absolute path to the model output file by combining subreddit, date_range and model name
+
+    Parameters
+    ----------
+    subreddit : str
+        subreddit name
+    date_range: list of DateTime objects
+        indicates the first and the last date of a period to be saved as a cache file
+    model_alias: str
+        name of the model (lda, nmf, bertopic)
+
+    Returns
+    -------
+    filepath : str
+        absolute path used to save the model output
+    """
     filename = str(subreddit) + '&' + str(date_range[0]) + '_' + str(date_range[1]) + '&'+ str(model_alias) +'.obj'
     return os.path.join(d.CACHE_DIR, filename)
 
 def check_cache_existance(subreddit, date_range, model_alias):
+    """
+    Check if cache file for given parameters exists (model output obtained from a given subreddit, date_range and model)
+
+    Parameters
+    ----------
+    subreddit : str
+        subreddit name
+    date_range: list of DateTime objects
+        the first and the last date of a posts creation period to be saved as a cache file
+    model_alias: str
+        name of the model (lda, nmf, bertopic)
+
+    Returns
+    -------
+    bool
+        whether the specified model has been already saved after training on a given subreddit for a chosen date range
+    """
     cached_df = pd.read_csv(os.path.join(d.CACHE_DIR, 'cached_files.csv'))
     return not cached_df.loc[(cached_df['subreddit'] == subreddit) & (cached_df['date_range_0'] == date_range[0]) & (cached_df['date_range_1'] == date_range[1]) & (cached_df['model'] == model_alias), :].empty
 
 
 def load_cache_output_data(filepath):
+    """
+    Load cached output data from a pickle file
+
+    Parameters
+    ----------
+    filepath : str
+        path to load the model output
+
+    Returns
+    -------
+    OutputData
+        result obtained from training a model
+    """
     with open(filepath, 'rb') as file:
         return pickle.load(file)
 
 
 def load_raw_data_and_preprocess(subreddit):
+    """
+    Load raw data for a subreddit and perform a preprocessing
+
+    Parameters
+    ----------
+    subreddit : str
+        subreddit to be preprocessed
+
+    Returns
+    -------
+    df_prep: DataFrame
+        preprocessed dataframe which contains lemmatized text from posts and posts creation date
+    """
     filepath = os.path.join(d.RAW_DIR, subreddit + '.csv')
     df_raw = pd.read_csv(filepath, sep=',')
     df_prep = DataPreprocessor().preprocess_dataframe(df_raw,
@@ -57,6 +132,21 @@ def load_raw_data_and_preprocess(subreddit):
 
 
 def download_new_subreddit_and_preprocess(subreddit, date_range):
+    """
+    Download a subreddit directly via Pushshift API and preprocess it to obtain lemmatized data
+
+    Parameters
+    ----------
+    subreddit : str
+        subreddit to be downloaded and preprocessed
+    date_range: list of DateTime objects
+        date ranges of posts creation dates to be downloaded
+
+    Returns
+    -------
+    df_prep: DataFrame
+        preprocessed dataframe which contains lematized text from posts and posts creation date
+    """
     df_raw = DataDownloader(verbose=True).download_data(subreddit,
                                                         start_date=str(date_range[0]),
                                                         end_date=str(date_range[1]),
@@ -71,7 +161,22 @@ def download_new_subreddit_and_preprocess(subreddit, date_range):
     return df_prep
 
 
-def load_raw_data(subreddits, date_range): #do użycia w eksploracji danych
+def load_raw_data(subreddits, date_range):
+    """
+    Load row data, which is needed in data exploration
+
+    Parameters
+    ----------
+    subreddits : str
+        subreddits to be loaded from raw directory data
+    date_range: list of DateTime objects
+        date ranges of posts to be downloaded
+
+    Returns
+    -------
+    result_df: DataFrame
+        dataframe which contains raw text from posts and posts creation date
+    """
     first = True
     subreddits = list(map(lambda x: str(x).lower(), subreddits))
     cwd = Path.cwd()
