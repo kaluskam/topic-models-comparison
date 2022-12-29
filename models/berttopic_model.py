6c68992ac16f3f59fe012ffbb36a3727064afbb3
Marcelina Kurek <marcelina.kurek1@gmail.com>
2022-12-29 19:23:10 +0100

    add topics reduction

--- models/berttopic_model.py
+++ models/berttopic_model.py
@@ -2,7 +2,7 @@ from msilib import init_database
 
 import numpy as np
 import pandas as pd
-
+import hdbscan
 from .model import Model
 from utils.data_structures import OutputData
 
@@ -19,11 +19,12 @@ class BERTopicModel(Model):
             self.init_default_parameters()
 
     def fit(self, data, n_topics = None):
+        n_topics = n_topics if n_topics is not None else 10
         super().fit(data)
         self.data = data
         bert = BERTopic(**self.parameters["bertopic"])
         self.topic_ids, _ = bert.fit_transform([str(text) for text in data.texts])
-
+        bert.reduce_topics([str(text) for text in data.texts], nr_topics=n_topics)
         self.model = bert
 
     def get_output(self):
@@ -43,7 +44,10 @@ class BERTopicModel(Model):
         self.output.add_texts_topics(np.arange(1, len(self.topic_ids) + 1), self.topic_ids)
 
     def init_default_parameters(self):
-        self.parameters = {"bertopic": {"n_gram_range": (1, 1)}}
+        clusterer = hdbscan.HDBSCAN(algorithm='best', approx_min_span_tree=True,
+                                    gen_min_span_tree=False, leaf_size=30, metric='euclidean', min_cluster_size=25,
+                                    min_samples=20, cluster_selection_epsilon=0.5, p=None)
+        self.parameters = {"bertopic": {"n_gram_range": (1, 1), "hdbscan_model": clusterer, "nr_topics": "auto"}}
 
     def save(self, filepath):
         super().save(filepath)
